4d95e24c32a068bb4fc4f4a19236ed838aea7ffd
NateKunsman <nkkuns@protonmail.com>
2021-11-23 22:02:16 -0500

    added transaction and shipper models

--- Koonlight.Models/ShipperEdit.cs
+++ Koonlight.Models/ShipperEdit.cs
@@ -1,20 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Koonlight.Models
 {
-    public class ShipperCreate
+    class ShipperEdit
     {
         [Key]
         public int ShipperID { get; set; }
-        public virtual Load Load { get; set; }
-        [ForeignKey(nameof(Load))]
-        public int LoadID { get; set; }
         [Required]
         public string CompanyName { get; set; }
         [Required]
---
+++ Koonlight.Models/TransactionCreate.cs
@@ -0,0 +1,27 @@
+﻿using Koonlight.MVC.Data;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Koonlight.Models
+{
+    public class TransactionCreate
+    {
+        [Key]
+        public int TransactionID { get; set; }
+        public int Payout { get; set; }
+        [ForeignKey(nameof(Shipper))]
+        public int ShipperID { get; set; }
+        public virtual Shipper Shipper { get; set; }
+        [ForeignKey(nameof(Load))]
+        public int LoadID { get; set; }
+        public virtual Load Load { get; set; }
+        [ForeignKey(nameof(Driver))]
+        public string DriverID { get; set; }
+        public virtual ApplicationUser Driver { get; set; }
+    }
+}
